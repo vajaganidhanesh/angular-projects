4c37dba8ff65d3fc0fa7f0a5832a8d8b10bd6616
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-04-21 22:26:55 +0530

    todo list

--- projects/todo-list/src/app/app.component.ts
+++ projects/todo-list/src/app/app.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === 'done' ? item.done : item.description
+    );
+  }
+
+  addItem(description: any) {
+    // this.allItems.unshift({
+    //   description,
+    //   done: false,
+    // });
+    console.log(description);
+  }
 }
