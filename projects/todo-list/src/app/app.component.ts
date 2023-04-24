8c8e5a9c873abeed0c48c4f43e49e74ae8754912
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-04-24 16:46:48 +0530

    Nested array of objects

--- projects/todo-list/src/app/app.component.ts
+++ projects/todo-list/src/app/app.component.ts
@@ -1,36 +1,51 @@
 import { Component } from '@angular/core';
-
+import { MyInterface } from './contracts/item';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo';
+  // title = 'todo';
 
-  filter: 'all' | 'active' | 'done' = 'all';
+  // filter: 'all' | 'active' | 'done' = 'all';
 
-  allItems = [
-    { description: 'eat', done: true },
-    { description: 'sleep', done: false },
-    { description: 'play', done: false },
-    { description: 'laugh', done: false },
+  // allItems = [
+  //   { description: 'eat', done: true },
+  //   { description: 'sleep', done: false },
+  //   { description: 'play', done: false },
+  //   { description: 'laugh', done: false },
+  // ];
+
+  // get items() {
+  //   if (this.filter === 'all') {
+  //     return this.allItems;
+  //   }
+  //   return this.allItems.filter((item) =>
+  //     this.filter === 'done' ? item.done : item.description
+  //   );
+  // }
+
+  // addItem(description: any) {
+  //   this.allItems.unshift({
+  //     description: description.User,
+  //     done: description.bool,
+  //   });
+  //   console.log(description);
+  // }
+
+  public myData: MyInterface[] = [
+    {
+      name: 'Skill-1',
+      values: [{ pvalue1: 1, pvalue2: 2, pvalue3: 3 }],
+    },
+    {
+      name: 'Skill-2',
+      values: [{ pvalue1: 1, pvalue2: 2, pvalue3: 3 }],
+    },
   ];
 
-  get items() {
-    if (this.filter === 'all') {
-      return this.allItems;
-    }
-    return this.allItems.filter((item) =>
-      this.filter === 'done' ? item.done : item.description
-    );
-  }
-
-  addItem(description: any) {
-    this.allItems.unshift({
-      description: description.User,
-      done: description.bool,
-    });
-    console.log(description);
+  constructor() {
+    console.log(this.myData);
   }
 }
--- projects/todo-list/src/app/contracts/item.ts
+++ projects/todo-list/src/app/contracts/item.ts
@@ -2,3 +2,12 @@ export interface Item {
   description: string;
   done: boolean;
 }
+
+export interface MyInterface {
+  name: string;
+  values: {
+    pvalue1: number;
+    pvalue2: number;
+    pvalue3: number;
+  }[];
+}
