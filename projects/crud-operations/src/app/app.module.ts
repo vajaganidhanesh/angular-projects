2fa81a062e979df90eef7291313565fd74b3bce3
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-05-02 09:41:08 +0530

    close button has been done in view feature

--- projects/crud-operations/src/app/app.module.ts
+++ projects/crud-operations/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewDataComponent } from './components/view-data/view-data.component';
+import { NewItemComponent } from './components/new-item/new-item.component';
 
 @NgModule({
-  declarations: [AppComponent, ViewDataComponent],
+  declarations: [AppComponent, ViewDataComponent, NewItemComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
